0c03e6d099db7e9188472adbeb6e419f94eacdac
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-07-27 14:07:23 +0530

    completed multiplayer functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-    
+socketio = SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
+
 from app import views
