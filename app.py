f9525b7cc1499b5685c3323796be14f4fcc56b56
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-07-27 16:30:06 +0530

    structured code files

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, send, emit
+import os
+import json
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
+        
+        
+@app.route("/") 
+def index():
+    return render_template("index.html")
+
+
+@socketio.on('json')
+def handleJson(jsonData):
+    emit('json', jsonData, broadcast=True)
+
+
+if __name__ == "__main__":
+    socketio.run(app)
--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_socketio import SocketIO, send
-
-app = Flask(__name__)
-socketio = SocketIO(app)
-socketio.init_app(app, cors_allowed_origins="*")
-
-from app import views
